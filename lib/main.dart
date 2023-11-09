92e0c0325df788ff4892bf58800ec407c5bbaa59
ShamsHamdan <shamshamdan71@gmail.com>
2023-11-09 22:32:27 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:first_version/features/Splash/presentation/views/SplashView.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+void main() {
+  runApp(const smartAttendance());
+}
+
+class smartAttendance extends StatelessWidget {
+  const smartAttendance({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: SplashView(),
+    );
+  }
+}
