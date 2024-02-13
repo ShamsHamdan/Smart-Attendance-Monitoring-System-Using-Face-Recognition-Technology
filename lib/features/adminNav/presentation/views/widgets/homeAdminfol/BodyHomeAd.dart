688cbbe39eace6364e1ab2eec0d1a56bee70281c
ShamsHamdan <shamshamdan71@gmail.com>
2024-02-13 21:33:13 +0200

    statistics button done

--- lib/features/adminNav/presentation/views/widgets/homeAdminfol/BodyHomeAd.dart
+++ lib/features/adminNav/presentation/views/widgets/homeAdminfol/BodyHomeAd.dart
@@ -1,45 +1,89 @@
-
+import 'package:flutter/material.dart';
 import 'package:first_version/constants.dart';
-import 'package:first_version/core/utils/assets.dart';
 import 'package:first_version/features/adminNav/presentation/views/widgets/homeAdminfol/AdminHeader.dart';
 import 'package:first_version/features/adminNav/presentation/views/widgets/homeAdminfol/QuickActions.dart';
 import 'package:first_version/features/adminNav/presentation/views/widgets/homeAdminfol/RecentlyTeacher.dart';
 import 'package:first_version/features/adminNav/presentation/views/widgets/homeAdminfol/RecntlyAddCourses.dart';
 import 'package:first_version/features/adminNav/presentation/views/widgets/homeAdminfol/adminchart.dart';
-import 'package:flutter/material.dart';
 import 'package:scroll_to_index/scroll_to_index.dart';
 
 class BodyHomeAdmin extends StatefulWidget {
-  
-  const BodyHomeAdmin({super.key});
+  const BodyHomeAdmin({Key? key}) : super(key: key);
 
   @override
-  State<BodyHomeAdmin> createState() => _BodyHomeAdminState();
+  _BodyHomeAdminState createState() => _BodyHomeAdminState();
 }
 
 class _BodyHomeAdminState extends State<BodyHomeAdmin> {
-
-    final ScrollController scrollController = ScrollController();
+  final AutoScrollController scrollController = AutoScrollController(); // Change here
+GlobalKey key = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-         controller: scrollController,
+      controller: scrollController,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-       children: [
-        HomeAdminHeader(),
-        QuickActions(scrollController: scrollController,),
-        SizedBox(
-          height: 30,
-        ),
-       RecentlyAddCourses(),
-        SizedBox(
-          height: 30,
-        ),
-         RecentlyAddTeachers(),
-         AdminChart(),
-      ]
+        children: [
+          HomeAdminHeader(),
+          QuickActions(scrollController: scrollController,),
+          SizedBox(
+            height: 30,
+          ),
+          RecentlyAddCourses(),
+          SizedBox(
+            height: 30,
+          ),
+          RecentlyAddTeachers(),
+          AdminChart(),
+        ],
       ),
     );
   }
 }
+
+
+
+// import 'package:first_version/constants.dart';
+// import 'package:first_version/core/utils/assets.dart';
+// import 'package:first_version/features/adminNav/presentation/views/widgets/homeAdminfol/AdminHeader.dart';
+// import 'package:first_version/features/adminNav/presentation/views/widgets/homeAdminfol/QuickActions.dart';
+// import 'package:first_version/features/adminNav/presentation/views/widgets/homeAdminfol/RecentlyTeacher.dart';
+// import 'package:first_version/features/adminNav/presentation/views/widgets/homeAdminfol/RecntlyAddCourses.dart';
+// import 'package:first_version/features/adminNav/presentation/views/widgets/homeAdminfol/adminchart.dart';
+// import 'package:flutter/material.dart';
+// import 'package:scroll_to_index/scroll_to_index.dart';
+
+// class BodyHomeAdmin extends StatefulWidget {
+  
+//   const BodyHomeAdmin({super.key});
+
+//   @override
+//   State<BodyHomeAdmin> createState() => _BodyHomeAdminState();
+// }
+
+// class _BodyHomeAdminState extends State<BodyHomeAdmin> {
+
+//     final ScrollController scrollController = ScrollController();
+//   @override
+//   Widget build(BuildContext context) {
+//     return SingleChildScrollView(
+//          controller: scrollController,
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//        children: [
+//         HomeAdminHeader(),
+//         QuickActions(scrollController: scrollController,),
+//         SizedBox(
+//           height: 30,
+//         ),
+//        RecentlyAddCourses(),
+//         SizedBox(
+//           height: 30,
+//         ),
+//          RecentlyAddTeachers(),
+//          AdminChart(),
+//       ]
+//       ),
+//     );
+//   }
+// }
--- lib/features/adminNav/presentation/views/widgets/homeAdminfol/QuickActions.dart
+++ lib/features/adminNav/presentation/views/widgets/homeAdminfol/QuickActions.dart
@@ -1,28 +1,49 @@
 import 'package:first_version/constants.dart';
 import 'package:first_version/features/adminNav/presentation/views/widgets/courseAdmin/add_course_form_page.dart';
+import 'package:first_version/features/adminNav/presentation/views/widgets/homeAdminfol/adminchart.dart';
 import 'package:first_version/features/adminNav/presentation/views/widgets/teacherAdmin/TabNavAddTeacherandCourse.dart';
 import 'package:first_version/features/adminNav/presentation/views/widgets/teacherAdmin/add_professor_form.dart';
 import 'package:first_version/features/adminNav/presentation/views/widgets/teacherAdmin/list_of_teacheres_Admin.dart';
 import 'package:flutter/material.dart';
+import 'package:scroll_to_index/scroll_to_index.dart';
 
-
-class  QuickActions extends StatelessWidget {
+class QuickActions extends StatelessWidget {
   final ScrollController scrollController;
-  const  QuickActions({super.key, required this.scrollController});
+  const QuickActions({super.key, required this.scrollController});
+ 
+  // void scrollToChart() {
+  //   // Access the RenderObject of the chart widget
+  //   final RenderBox? chartBox = _chartKey.currentContext?.findRenderObject() as RenderBox?;
+  //   if (chartBox != null) {
+  //     // Calculate the position of the chart widget within the SingleChildScrollView
+  //     final chartPosition = chartBox.localToGlobal(Offset.zero, ancestor: context.findRenderObject());
+  //     // Scroll to the position of the chart widget
+  //     scrollController.animateTo(
+  //       chartPosition.dy, // Use dy for vertical scrolling
+  //       duration: Duration(milliseconds: 500),
+  //       curve: Curves.easeInOut,
+  //     );
+  //   }
+  // }
+  
+ 
+
+
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.only(bottom: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [   GestureDetector(
+      margin: EdgeInsets.only(bottom: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          GestureDetector(
             onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => AddProfessorForm()));
-                //Navigator.pushNamed(context, "/maintabcoursesinteacher");
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => AddProfessorForm()));
+              //Navigator.pushNamed(context, "/maintabcoursesinteacher");
             },
-            child:
-            Column(
+            child: Column(
               children: [
                 Container(
                   decoration: BoxDecoration(
@@ -43,7 +64,6 @@ class  QuickActions extends StatelessWidget {
                   ),
                 ),
                 SizedBox(height: 7),
-             
                 Text(
                   "Add Teacher",
                   style: TextStyle(
@@ -52,22 +72,21 @@ class  QuickActions extends StatelessWidget {
                   ),
                 ),
               ],
-            ),  ),
-             GestureDetector(
+            ),
+          ),
+          GestureDetector(
             onTap: () {
-               Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) =>
-                                 AddTeacherandCourseInAdminTabNav(
-                              initialIndex: 1,
-                            ),
-                          ),
-                        ); 
-                //Navigator.pushNamed(context, "/maintabcoursesinteacher");
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AddTeacherandCourseInAdminTabNav(
+                    initialIndex: 1,
+                  ),
+                ),
+              );
+              //Navigator.pushNamed(context, "/maintabcoursesinteacher");
             },
-            child:
-            Column(
+            child: Column(
               children: [
                 Container(
                   decoration: BoxDecoration(
@@ -96,54 +115,30 @@ class  QuickActions extends StatelessWidget {
                   ),
                 ),
               ],
-            ),),
-          //   Column(
-          //   children: [
-          //     Container(
-          //       decoration: BoxDecoration(
-          //         borderRadius: BorderRadius.circular(50),
-          //         border: Border.all(
-          //           color: const Color(0xFFE4E7EC),
-          //           width: 2,
-          //         ),
-          //       ),
-          //       child: const CircleAvatar(
-          //         backgroundColor: Color.fromRGBO(255, 255, 255, 1),
-          //         radius: 30,
-          //         child: Icon(
-          //           Icons.bar_chart,
-          //           color: kPrimaryColor,
-          //           size: 30,
-          //         ),
-          //       ),
-          //     ),
-          //     const SizedBox(height: 7),
-          //     ElevatedButton(
-          //       onPressed: () {
-          //         // // Scroll to the bottom of the page where the chart is
-          //         // scrollController.animateTo(
-          //         //   scrollController.position.maxScrollExtent,
-          //         //   duration: const Duration(seconds: 2),
-          //         //   curve: Curves.easeInOut,
-          //         // );
-          //       },
-          //       child:  Text(
-          //         "Statistics",
-          //         style: TextStyle(
-          //           color: Color.fromARGB(255, 0, 0, 0),
-          //           fontSize: 17,
-          //         ),
-          //       ),
-          //     ),
-          //   ],
-          // ), 
-            Column(
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+             // (AdminChart context) => context.scrollToChart();
+              Scrollable.ensureVisible(
+                context,
+                duration: Duration(seconds: 2),
+                curve: Curves.easeInOut,
+                alignment: -1,
+              );
+              // scrollController.animateTo(
+              //   scrollController.position.maxScrollExtent,
+              //   duration: Duration(seconds: 1),
+              //   curve: Curves.easeInOut,
+              // );
+            },
+            child: Column(
               children: [
                 Container(
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(50),
                     border: Border.all(
-                      color: Color(0xFFE4E7EC),
+                      color: const Color(0xFFE4E7EC),
                       width: 2,
                     ),
                   ),
@@ -158,7 +153,7 @@ class  QuickActions extends StatelessWidget {
                   ),
                 ),
                 SizedBox(height: 7),
-                const Text(
+                Text(
                   "Statistics",
                   style: TextStyle(
                     color: Color.fromARGB(255, 0, 0, 0),
@@ -167,8 +162,79 @@ class  QuickActions extends StatelessWidget {
                 ),
               ],
             ),
-          ],
-        ),
-      );
+          ),
+
+          //   GestureDetector(
+          //   onTap: () {
+          //     scrollController.animateTo(
+          //       scrollController.position.maxScrollExtent,
+          //       duration: const Duration(seconds: 2),
+          //       curve: Curves.easeInOut,
+          //     );
+          //   },
+          //   child: Column(
+          //     children: [
+          //       Container(
+          //         decoration: BoxDecoration(
+          //           borderRadius: BorderRadius.circular(50),
+          //           border: Border.all(
+          //             color: const Color(0xFFE4E7EC),
+          //             width: 2,
+          //           ),
+          //         ),
+          //         child: const CircleAvatar(
+          //           backgroundColor: Color.fromRGBO(255, 255, 255, 1),
+          //           radius: 30,
+          //           child: Icon(
+          //             Icons.bar_chart,
+          //             color: kPrimaryColor,
+          //             size: 30,
+          //           ),
+          //         ),
+          //       ),
+          //       const SizedBox(height: 7),
+          //       const Text(
+          //         "Statistics",
+          //         style: TextStyle(
+          //           color: Color.fromARGB(255, 0, 0, 0),
+          //           fontSize: 17,
+          //         ),
+          //       ),
+          //     ],
+          //   ),
+          // ),
+          // Column(
+          //   children: [
+          //     Container(
+          //       decoration: BoxDecoration(
+          //         borderRadius: BorderRadius.circular(50),
+          //         border: Border.all(
+          //           color: Color(0xFFE4E7EC),
+          //           width: 2,
+          //         ),
+          //       ),
+          //       child: CircleAvatar(
+          //         backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
+          //         radius: 30,
+          //         child: Icon(
+          //           Icons.bar_chart,
+          //           color: kPrimaryColor,
+          //           size: 30,
+          //         ),
+          //       ),
+          //     ),
+          //     SizedBox(height: 7),
+          //     const Text(
+          //       "Statistics",
+          //       style: TextStyle(
+          //         color: Color.fromARGB(255, 0, 0, 0),
+          //         fontSize: 17,
+          //       ),
+          //     ),
+          //   ],
+          // ),
+        ],
+      ),
+    );
   }
 }
